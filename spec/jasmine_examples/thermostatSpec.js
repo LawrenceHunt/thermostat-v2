b70372c1e097b2c0deedfbb5fab32a1f6be54dda
cloudheadboatman <adam.boatman@gmail.com>
2017-07-22 16:45:46 +0100

    One passing test for display temp

---
+++ spec/jasmine_examples/thermostatSpec.js
@@ -0,0 +1,15 @@
+
+describe("Thermostat", function() {
+  var Thermostat = require('../../lib/thermostat/Thermostat');
+  var thermostat;
+
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it("should be set to 20💩", function() {
+    expect(thermostat.displayTemp()).toEqual(20);
+  });
+
+});
