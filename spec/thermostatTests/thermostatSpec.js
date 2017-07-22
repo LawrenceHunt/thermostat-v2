0fa8d48ac70af12768b7c21645f3839039a22c9e
Lawrence Hunt <lawrence.hunt@hotmail.com>
2017-07-22 16:55:02 +0100

    failing test written for increaseByOne

--- spec/thermostatTests/thermostatSpec.js
+++ spec/thermostatTests/thermostatSpec.js
@@ -1,4 +1,3 @@
-
 describe("Thermostat", function() {
   var Thermostat = require('../../lib/thermostat/Thermostat');
   var thermostat;
@@ -11,5 +10,12 @@ describe("Thermostat", function() {
   it("should be set to 20💩", function() {
     expect(thermostat.displayTemp()).toEqual(20);
   });
+
+  it("should increase the temp on push up button", function() {
+    var temp1 = thermostat.displayTemp();
+    thermostat.increaseByOne();
+    var temp2 = thermostat.displayTemp();
+    expect(temp1).not.toEqual(temp2);
+  })
 
 });
